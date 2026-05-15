702c3090ae6e5e56ca21263999bf1eb71c9925cc
mingdodev <mingdo.dev@gmail.com>
2026-05-16 02:05:36 +0900

    프로젝트 초기 세팅
    - 의존성 설정

---
+++ src/test/java/com/example/classregistration/ClassRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
